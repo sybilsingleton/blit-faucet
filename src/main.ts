40bf6a48b5bdb005126c26f9513122c25528169f
Your Name <you@example.com>
2023-12-01 10:59:00 +0000

    basic faucet logic and ui

--- src/main.ts
+++ src/main.ts
@@ -14,23 +14,21 @@ declare global {
     queryFunction: any;
   }
 }
+// @ts-ignore
+BigInt.prototype['toJSON'] = function () {
+  return this.toString();
+};
+import { default as blitjs, experimentalHelpers } from "@blitchain/blitjs";
 
-import {
-  default as blitjs,
-  experimentalHelpers,
-} from "@blitchain/blitjs";
-
-let {
-  makeKeplrClient,
-  makeJsClient,
-  runFunction,
-  queryFunction
-} = experimentalHelpers;
+let { makeKeplrClient, makeJsClient, runFunction, queryFunction } =
+  experimentalHelpers;
 
 let rpcEndpoint = "http://localhost:26657";
 let restEndpoint = "http://localhost:1317";
 
-let queryClient = await blitjs.blit.ClientFactory.createLCDClient({ restEndpoint });
+let queryClient = await blitjs.blit.ClientFactory.createLCDClient({
+  restEndpoint,
+});
 
 let msgClient = await makeKeplrClient({ rpcEndpoint, restEndpoint });
 
@@ -43,11 +41,13 @@ let balanceResponse = await queryClient.cosmos.bank.v1beta1.allBalances({
 console.log("keplr address", address);
 console.log("keplr balance:", balanceResponse.balances[0]);
 
-
-const mnemonic = "rhythm snake innocent moon husband gossip man toe industry senior essence summer traffic since parent thing limit void add perfect vague undo lecture flame";
+const mnemonic =
+  "rhythm snake innocent moon husband gossip man toe industry senior essence summer traffic since parent thing limit void add perfect vague undo lecture flame";
 let jsClient = await makeJsClient({ rpcEndpoint, restEndpoint, mnemonic });
 // @ts-ignore
-let jsAddress = (await jsClient.signer.getAccounts())[0].address
+jsClient.gasPrice = "0ublit";
+// @ts-ignore
+let jsAddress = (await jsClient.signer.getAccounts())[0].address;
 console.log("js address", jsAddress);
 let jsBalanceResponse = await queryClient.cosmos.bank.v1beta1.allBalances({
   address: jsAddress,
